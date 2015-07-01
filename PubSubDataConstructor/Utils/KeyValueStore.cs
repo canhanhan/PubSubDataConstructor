59ec508a09ec42f3e96f396b6d38ae4488942b7b
Can Hanhan <p_github@finarfin.net>
2015-07-01 21:01:08 +0200

    Namespace change

---
+++ PubSubDataConstructor/Utils/KeyValueStore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PubSubDataConstructor.Utils
+{
+    public class KeyValueStore<TKey, TValue> : Dictionary<TKey, List<TValue>>
+    {        
+        public void Add(TKey key, TValue value)
+        {
+            List<TValue> values;
+            if (!TryGetValue(key, out values))
+            {
+                values = new List<TValue>();
+                Add(key, values);
+            }
+
+            values.Add(value);
+        }
+
+        public void Remove(TKey key, TValue value)
+        {
+            List<TValue> values;
+            if (!TryGetValue(key, out values))
+                throw new KeyNotFoundException();
+
+            values.Remove(value);
+        }
+
+        public void Remove(TValue value)
+        {
+            foreach (var list in Values.Where(x => x.Contains(value)))
+                list.Remove(value);
+        }
+    }
+}
